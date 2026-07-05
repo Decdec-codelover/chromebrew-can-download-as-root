985adbdadb9b8124f79feaff23d8e732d16ddd38
Declan Pangburn <Declan.pangburn@icloud.com>
2026-07-05 11:56:48 -0400

    Implement Autotools build system class

---
+++ lib/buildsystems/autotools.rb
@@ -0,0 +1,64 @@
+require 'fileutils'
+require_relative '../package'
+require_relative '../require_gem'
+require_relative '../report_buildsystem_methods'
+
+class Autotools < Package
+  boolean_property :autotools_make_j1, :autotools_skip_configure
+  property :autotools_build_relative_dir, :autotools_configure_options, :autotools_configure_modifications, :autotools_install_options, :autotools_pre_configure_options, :autotools_build_extras, :autotools_install_extras, :autotools_pre_make_options, :autotools_make_options
+
+  def self.prebuild_config_and_report
+    @autotools_build_relative_dir ||= '.'
+    @autotools_install_options ||= "DESTDIR=#{CREW_DEST_DIR}"
+    @autotools_install_options = @autotools_install_options.split.unshift("DESTDIR=#{CREW_DEST_DIR}").uniq.join(' ')
+    @crew_autotools_configure_options = defined?(@autotools_configure_modifications) && @autotools_configure_modifications.is_a?(Array) ? CREW_CONFIGURE_OPTIONS.gsub(@autotools_configure_modifications[0], @autotools_configure_modifications[1]) : CREW_CONFIGURE_OPTIONS
+
+    extend ReportBuildsystemMethods
+
+    print_buildsystem_methods
+  end
+
+  def self.build
+    Dir.chdir(@autotools_build_relative_dir) do
+      # Some packages, such as ocaml, have a dummy makefile, so check that this is actually a generated makefile.
+      unless @autotools_skip_configure || (File.file?('Makefile') && File.read('Makefile', 500).include?('generated') && CREW_CACHE_BUILD)
+        if File.executable? './autogen.sh'
+          system 'NOCONFIGURE=1 ./autogen.sh --no-configure || NOCONFIGURE=1 ./autogen.sh'
+        elsif File.executable? './bootstrap'
+          system 'NOCONFIGURE=1 ./bootstrap --no-configure || NOCONFIGURE=1 ./bootstrap'
+        end
+        # This ensures the correct aclocal and automake versions are configured.
+        system 'autoreconf -fiv' if File.file?('configure.ac')
+        abort 'configure script not found!'.lightred unless File.file?('configure')
+        FileUtils.chmod('+x', 'configure')
+        system 'filefix', exception: false unless @no_filefix
+        system "#{@autotools_pre_configure_options} ./configure #{@crew_autotools_configure_options} #{@autotools_configure_options}"
+      end
+
+      # Add "-j#" argument to "make" at compile-time, if necessary.
+      if @autotools_make_j1
+        system "#{@autotools_pre_make_options} make -j1 #{@autotools_make_options}"
+      else
+        system "#{@autotools_pre_make_options} make -j#{CREW_NPROC} #{@autotools_make_options}"
+      end
+
+      @autotools_build_extras&.call
+    end
+  end
+
+  def self.install
+    Dir.chdir(@autotools_build_relative_dir) do
+      system "make #{@autotools_install_options} install"
+      @autotools_install_extras&.call
+    end
+  end
+
+  def self.check
+    return unless @run_tests
+
+    Dir.chdir(@autotools_build_relative_dir) do
+      puts 'Testing with make check.'.orange
+      system "make #{@autotools_install_options} check"
+    end
+  end
+end
